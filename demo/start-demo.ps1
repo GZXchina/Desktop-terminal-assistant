be413a8d59cc0ef93f5f3b44e64baedf5816ca9e
GZXchina <150024881+GZXchina@users.noreply.github.com>
2026-05-18 01:23:04 +0800

    Delete demo directory

--- demo/start-demo.ps1
+++
@@ -1,8 +0,0 @@
-# 启动工位搭子 Web Demo
-$demoPath = Join-Path $PSScriptRoot "index.html"
-if (-not (Test-Path $demoPath)) {
-    Write-Error "找不到 demo/index.html"
-    exit 1
-}
-Write-Host "正在打开工位搭子 Demo: $demoPath"
-Start-Process $demoPath
